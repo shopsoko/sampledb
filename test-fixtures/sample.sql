edeb0ca8f2d63c2bdee678417515ec1bed2e1ba6
freddieptf <fred@shopsoko.com>
2020-08-31 18:23:57 +0300

    fix: sampledb test

---
+++ test-fixtures/sample.sql
@@ -0,0 +1,42 @@
+DROP DATABASE IF EXISTS sample;
+CREATE DATABASE IF NOT EXISTS sample;
+use sample;
+
+CREATE TABLE employees(
+    emp_no      INT             NOT NULL,
+    birth_date  DATE            NOT NULL,
+    first_name  VARCHAR(14)     NOT NULL,
+    last_name   VARCHAR(16)     NOT NULL,
+    gender      ENUM ('M','F')  NOT NULL,    
+    hire_date   DATE            NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE departments (
+    dept_no     CHAR(4)         NOT NULL,
+    dept_name   VARCHAR(40)     NOT NULL,
+    PRIMARY KEY (dept_no),
+    UNIQUE  KEY (dept_name)
+);
+
+CREATE TABLE dept_emp (
+    emp_no      INT             NOT NULL,
+    dept_no     CHAR(4)         NOT NULL,
+    from_date   DATE            NOT NULL,
+    to_date     DATE            NOT NULL,
+    FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no)  ON DELETE CASCADE,
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
+    PRIMARY KEY (emp_no,dept_no)
+);
+
+INSERT INTO `employees` VALUES (10001,'1953-09-02','Georgi','Facello','M','1986-06-26'),
+(10002,'1964-06-02','Bezalel','Simmel','F','1985-11-21'),
+(10003,'1959-12-03','Parto','Bamford','M','1986-08-28');
+
+INSERT INTO `departments` VALUES 
+('d001','Marketing'),
+('d002','Finance');
+
+INSERT INTO `dept_emp` VALUES (10001,'d001','1986-06-26','9999-01-01'),
+(10002,'d001','1996-08-03','9999-01-01'),
+(10003,'d002','1995-12-03','9999-01-01');
